0b340a9fcfd644110788a455b042a65797fa59a0
Tyler Sloan <tcsloan44@gmail.com>
2024-05-09 18:32:35 -0400

    finished with all of the jsx, still need to style

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,29 @@ import {puppyList} from './data.js'
 
 function App() {
   const [puppies, setPuppies]= useState(puppyList);
+  const [featPupId, setFeatPupId] = useState(null);
   console.log(puppies);
+
   return (
     <>
     <div className="App">{
       puppies.map((puppy)=>{
-        return <p key={puppy.id}>{puppy.name}</p>
-      })
-      }
+
+        return <p onClick={()=>{
+          setFeatPupId(puppy.id);
+          const featuredPup = puppies.find((pup)=> pup.id ===featPupId);
+          console.log(featuredPup);
+        }} key={puppy.id}>{puppy.name}</p>
+      })}
+      { featPupId && (
+        <div>
+          <h2>{featuredPup.name}</h2>
+          <ul>
+            <li>Age: {featuredPup.age}</li>
+            <li>Email: {featuredPup.email}</li>
+          </ul>
+        </div>
+      )}
     </div>
     </>
   )
