4c8e336e640959ac6f468e0108298e56a2f67d2a
Tyler Sloan <tcsloan44@gmail.com>
2024-05-09 18:53:09 -0400

    commit not working

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import './App.css'
 import {puppyList} from './data.js'
 
+
 function App() {
   const [puppies, setPuppies]= useState(puppyList);
   const [featPupId, setFeatPupId] = useState(null);
